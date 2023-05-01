e9bb9eac03b06b61b842b1fa8c0672162f92e002
Victor Lahr <lahr.victor@gmail.com>
2023-05-01 13:01:04 -0300

    fix: replace field productId by id on checkout POST request

--- src/schemas/cart.schema.js
+++ src/schemas/cart.schema.js
@@ -4,15 +4,15 @@ import joi from 'joi';
 // VALUE EXPORTS
 
 export const cartSchema = joi.object(
-    {
-        items: joi.array().items(
-            {
-                productId: joi.string().optional(),
-                title: joi.string().required(),
-                price: joi.number().positive().greater(0).required(),
-                qty: joi.number().positive().greater(0).required(),
-            }        
-        ),
-        total: joi.number().positive().greater(0).required(),
-    }
+  {
+    items: joi.array().items(
+      {
+        id: joi.string().optional(),
+        title: joi.string().required(),
+        price: joi.number().positive().greater(0).required(),
+        qty: joi.number().positive().greater(0).required(),
+      }
+    ),
+    total: joi.number().positive().greater(0).required(),
+  }
 )
